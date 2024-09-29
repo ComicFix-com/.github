0de68d58c8d9ebcc2c3960d2621cef467642ba38
Muhammed ALi <155841427+adilali097@users.noreply.github.com>
2024-09-29 17:35:41 +0530

    Create LICNSE.d

---
+++ profile/LICNSE.d
@@ -0,0 +1,15 @@
+Copyright 2024 ComicFix Community
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+ComicFix is a registered trademark of ComicFix Community, a non-profit organization supporting open-source projects for developers.
